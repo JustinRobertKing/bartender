d03c4c9cc7ecf1b43d0047cb07e128de64a70d7f
JustinRobertKing <kingj4@spu.edu>
2019-04-17 12:01:21 -0700

    created drink, ingredient, and drinks_ingredients models

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -8,8 +8,24 @@ let router = express.Router()
 // let db = require('../models')
 
 // Declare routs
+// GET /drinks
 router.get('/', (req, res) => {
-	res.send('Profile')
+	res.render('drinks/index')
+})
+
+// POST /drinks
+router.post('/', (req, res) => {
+	res.send(req.body)	
+})
+
+// GET /drinks/new
+router.get('/new', (req, res) => {
+	res.render('drinks/new')
+})
+
+// GET /drinks/:id
+router.get('/', (req, res) => {
+	res.render('drinks/show')	
 })
 
 // Export the routes from this file
